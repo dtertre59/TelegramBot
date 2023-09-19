962df00a2c47ac2d494dd242d03c2e2c0d8a5095
David Tertre <d.tertre@alumnos.upm.es>
2023-09-19 11:19:54 +0200

    FIRST COMMAND /START

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -1,11 +1,31 @@
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
-
 # Acceder a las variables de entorno
 import os
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 print(TELEGRAM_BOT_TOKEN)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
